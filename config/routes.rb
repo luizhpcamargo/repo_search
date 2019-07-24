fc091d27bd866e5432a2f28bc5191e82a8daf582
Luiz Camargo <luizhp.camargo@gmail.com>
2019-07-24 18:27:53 -0300

    chore: some updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'repositories#index'
+
+  resources :repositories, only: :index do
+    collection do
+      post :update_data
+    end
+  end
 end
